faf2d457eb5fecc829ff5367a746c5f85f008acc
AditiKhare25 <aditikhare25404@gmail.com>
2023-07-11 23:43:42 -0700

    package

---
+++ Package/package1.java
@@ -0,0 +1,9 @@
+
+package Mypack;
+import a.test;
+public class package1 {
+    public static void main(String [] args){
+        test ob=new test();
+        ob.sayhello();
+    }
+}
---
+++ Package/test.java
@@ -0,0 +1,8 @@
+package a;
+public class test{
+    
+    public void sayhello(){
+        System.out.println("from package mypack");
+    }
+}
+
